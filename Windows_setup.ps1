6dc93b086cf96e4cba94c02576543514042088ed
Siddharth Sangwan <36560000+SNGWN@users.noreply.github.com>
2023-07-23 22:01:38 +0530

    Update Windows_setup.ps1

--- Windows_setup.ps1
+++ Windows_setup.ps1
@@ -1,13 +1,14 @@
 # Name is Important
-echo "
-                #####    #     #    #####    #     #   #     # 
-               #     #   ##    #   #     #   #     #   ##    # 
-               #         # #   #   #         #     #   # #   # 
-                #####    #  #  #   #  ####   #  #  #   #  #  # 
-                     #   #   # #   #     #   #  #  #   #   # # 
-               #     #   #    ##   #     #   #  #  #   #    ## 
-                #####    #     #    #####     ## ##    #     # 
-"
+echo '
+ .d8888b.          888b    888          .d8888b.          888       888         888b    888 
+d88P  Y88b         8888b   888         d88P  Y88b         888       888         8888b   888 
+Y88b.              88888b  888         888    888         888  d8b  888         88888b  888 
+ "Y888b.           888Y88b 888         888                888 d888b 888         888Y88b 888 
+    "Y88b.         888 Y88b888         888  88888         888d88888b888         888 Y88b888 
+      "888         888  Y88888         888    888         88888P Y88888         888  Y88888 
+Y88b  d88P         888   Y8888         Y88b  d88P         8888P   Y8888         888   Y8888 
+ "Y8888P"          888    Y888          "Y8888P88         888P     Y888         888    Y888 
+'
 
 # Set Wget Progress to Silent, Becuase it slows down Downloading by +50x
 echo "Setting Wget Progress to Silent, Becuase it slows down Downloading by +50x`n"
@@ -43,19 +44,19 @@ if (!($jre8)){
 if (Test-Path Burp-Suite-Pro.jar){
     echo "Burp Suite Professional JAR file is available.`nChecking its Integrity ...."
     if (((Get-Item Burp-Suite-Pro.jar).length/1MB) -lt 500 ){
-        echo "`n`t`tFiles Seems to be corrupted `n`t`tDownloading Burp Suite Professional v2022.8.2 ...."
-        wget "https://portswigger.net/burp/releases/startdownload?product=pro&version=2022.8.2&type=Jar" -O "Burp-Suite-Pro.jar"
+        echo "`n`t`tFiles Seems to be corrupted `n`t`tDownloading Burp Suite Professional latest version ...."
+        wget "https://portswigger.net/burp/releases/startdownload?product=pro&version=&type=Jar" -O "burpsuite_pro.jar"
         echo "`nBurp Suite Professional is Downloaded.`n"
     }else {echo "File Looks fine. Lets proceed for Execution"}
 }else {
-    echo "`n`t`tDownloading Burp Suite Professional v2022.8.2 ...."
-    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.8.2&type=jar" -O "Burp-Suite-Pro.jar"
+    echo "`n`t`tDownloading Burp Suite Professional latest version ...."
+    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O "burpsuite_pro.jar"
     echo "`nBurp Suite Professional is Downloaded.`n"
 }
 
 # Creating Burp.bat file with command for execution
 if (Test-Path burp.bat) {rm burp.bat} 
-$path = "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:`"$pwd\loader.jar`" -noverify -jar `"$pwd\Burp-Suite-Pro.jar`""
+$path = "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:`"$pwd\New_loader.jar`" -noverify -jar `"$pwd\burpsuite_pro.jar`""
 $path | add-content -path Burp.bat
 echo "`nBurp.bat file is created"
 
@@ -76,13 +77,14 @@ del -Recurse -Force .\.github\
 # Lets Activate Burp Suite Professional with keygenerator and Keyloader
 echo "Reloading Environment Variables ...."
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-echo "`n`nStarting Keygenerator ...."
-start-process java.exe -argumentlist "-jar keygen.jar"
+echo "`n`nExecuting Keygenerator ...."
+start-process java.exe -argumentlist "-jar New_loader.jar"
 echo "`n`nStarting Burp Suite Professional"
-java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "Burp-Suite-Pro.jar"
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"New_loader.jar" -noverify -jar "burpsuite_pro.jar"
 
 
 # Lets Download the latest Burp Suite Professional jar File
+echo "`n`t`t Follow below steps to update Burp Suite Professional :-:"
 echo "`n`t`t 1. Please download latest Burp Suite Professional Jar file from :-:"
 echo "`n`t`t https://portswigger.net/burp/releases/startdownload?product=pro&version=&type=Jar"
 echo "`n`t`t 2. Replace the existing Burp-Suite-Pro.jar file with downloaded jar file. `n`t Keep previous file name"
