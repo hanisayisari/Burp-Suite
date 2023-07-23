f8f6618d8482fc5c5546ef71848ed9468c0524e9
Siddharth Sangwan <36560000+SNGWN@users.noreply.github.com>
2023-07-24 00:17:09 +0530

    Update Windows_setup.ps1

--- Windows_setup.ps1
+++ Windows_setup.ps1
@@ -82,7 +82,7 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 echo "`n`nExecuting Keygenerator ...."
 start-process java.exe -argumentlist "-jar New_loader.jar"
 echo "`n`nStarting Burp Suite Professional"
-java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"New_loader.jar" -noverify -jar "burpsuite_pro.jar"
+java "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" -javaagent:"$pwd\New_loader.jar" -noverify -jar "$pwd\burpsuite_pro.jar"
 
 
 # Lets Download the latest Burp Suite Professional jar File
